a6c608a8160bc49a9c7e7bd0d9549277116bf22e
Figaarillo <axel.leonardi.22@gmail.com>
2023-09-04 00:54:58 -0300

    feat: add hook commit lint

--- src/user/infrastructure/repositories/user-in-memory.repository.ts
+++ src/user/infrastructure/repositories/user-in-memory.repository.ts
@@ -2,4 +2,6 @@ import type IUserEntity from '@user/domain/interfaces/user.entity.interface'
 import type IUserRepository from '../interfaces/user.repository.interface'
 import BaseInMemoryRepository from '@shared/infrastructure/repositories/in-memory/base.in-memory.repository'
 
-export class UserInMemoryRepository extends BaseInMemoryRepository<IUserEntity> implements IUserRepository { }
+export class UserInMemoryRepository
+  extends BaseInMemoryRepository<IUserEntity>
+  implements IUserRepository {}
