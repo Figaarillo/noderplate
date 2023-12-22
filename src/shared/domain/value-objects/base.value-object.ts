1a67f09822a99f2f76c7ab5d51a3859b0c35286f
Figaarillo <axel.leonardi.22@gmail.com>
2023-12-22 04:12:29 -0300

    feat: add uuid

--- src/shared/domain/value-objects/base.value-object.ts
+++ src/shared/domain/value-objects/base.value-object.ts
@@ -1,8 +1,10 @@
+import { v4 as uuidv4 } from 'uuid'
+
 export class Id {
   private readonly _value: string
 
   constructor() {
-    this._value = String(Date.now())
+    this._value = uuidv4()
   }
 
   get value(): string {
