93260f6b1135c2c7a0bf1deb9b571c030674c860
Figaarillo <axel.leonardi.22@gmail.com>
2023-12-24 18:18:03 -0300

    feat: add emial value object

---
+++ src/user/domain/value-objects/email.value-object.ts
@@ -0,0 +1,32 @@
+class Email {
+  constructor(private readonly _value: string) {
+    this.ensureValueIsNotEmpty(_value)
+    this.ensureValueIsValidEmail(_value)
+    this._value = _value
+  }
+
+  private ensureValueIsNotEmpty(value: string | undefined | null): void {
+    if (value === undefined || value === null || value === '') {
+      // TODO: create a custom error
+      throw new Error('Value cannot be empty')
+    }
+  }
+
+  private ensureValueIsValidEmail(value: string): void {
+    if (value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) == null) {
+      // TODO: create a custom error
+      throw new Error('Invalid email address')
+    }
+  }
+
+  // TODO: Validar unicidad. No puedes tener dos usuarios con el mismo correo electrónico.
+  // TODO: Validar que el correo electrónico sea de un dominio válido. Por ejemplo, solo empleados de la empresa pueden registrarse con correos de la empresa
+  // TODO: Validar que el correo electrónico no esté registrado en una lista negra.
+  // TODO: Validar que el usuario sea el propietario del correo electrónico mediante un enlace de verificación.
+
+  get value(): string {
+    return this._value
+  }
+}
+
+export default Email
