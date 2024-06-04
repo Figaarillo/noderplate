5db9e41583481c6329dc67cef8f4ec382d89d800
Figaarillo <axel.leonardi.22@gmail.com>
2024-06-04 15:51:59 -0300

    refactor: simplify user entity structure

--- src/user/domain/entities/user.entity.ts
+++ src/user/domain/entities/user.entity.ts
@@ -13,74 +13,106 @@ import {
 } from '../value-objects/user.valueobject'
 
 class UserEntity {
-  private readonly id: Id = new Id()
-  private readonly createdAt: CreateAt = new CreateAt()
-  private updatedAt: UpdateAt = new UpdateAt()
-  private firstName: FirstName
-  private lastName: LastName
-  private email: Email
-  private readonly password: Password
-  private phoneNumber: PhoneNumber
-  private city: City
-  private province: Province
-  private conuntry: Country
-  private role: Role
+  private readonly _id: Id = new Id()
+  private readonly _createdAt: CreateAt = new CreateAt()
+  private _updatedAt: UpdateAt = new UpdateAt()
+  private _firstName: FirstName
+  private _lastName: LastName
+  private _email: Email
+  private readonly _password: Password
+  private _phoneNumber: PhoneNumber
+  private _city: City
+  private _province: Province
+  private _conuntry: Country
+  private _role: Role
 
   constructor(data: UserDTO) {
-    this.firstName = new FirstName(data.firstName)
-    this.lastName = new LastName(data.lastName)
-    this.email = new Email(data.email)
-    this.password = new Password(data.password)
-    this.phoneNumber = new PhoneNumber(data.phoneNumber)
-    this.city = new City(data.city)
-    this.province = new Province(data.province)
-    this.conuntry = new Country(data.country)
-    this.role = new Role(data.role)
+    this._firstName = new FirstName(data.firstName)
+    this._lastName = new LastName(data.lastName)
+    this._email = new Email(data.email)
+    this._password = new Password(data.password)
+    this._phoneNumber = new PhoneNumber(data.phoneNumber)
+    this._city = new City(data.city)
+    this._province = new Province(data.province)
+    this._conuntry = new Country(data.country)
+    this._role = new Role(data.role)
   }
 
   update(data: UserDTO): void {
-    this.updatedAt = new UpdateAt()
+    this._updatedAt = new UpdateAt()
     if (data.firstName !== undefined) {
-      this.firstName = new FirstName(data.firstName)
+      this._firstName = new FirstName(data.firstName)
     }
     if (data.lastName !== undefined) {
-      this.lastName = new LastName(data.lastName)
+      this._lastName = new LastName(data.lastName)
     }
     if (data.email !== undefined) {
-      this.email = new Email(data.email)
+      this._email = new Email(data.email)
     }
     if (data.phoneNumber !== undefined) {
-      this.phoneNumber = new PhoneNumber(data.phoneNumber)
+      this._phoneNumber = new PhoneNumber(data.phoneNumber)
     }
     if (data.city !== undefined) {
-      this.city = new City(data.city)
+      this._city = new City(data.city)
     }
     if (data.province !== undefined) {
-      this.province = new Province(data.province)
+      this._province = new Province(data.province)
     }
     if (data.country !== undefined) {
-      this.conuntry = new Country(data.country)
+      this._conuntry = new Country(data.country)
     }
     if (data.role !== undefined) {
-      this.role = new Role(data.role)
+      this._role = new Role(data.role)
     }
   }
 
-  getDTO(): UserDTO {
-    return {
-      id: this.id.value,
-      firstName: this.firstName.value,
-      lastName: this.lastName.value,
-      email: this.email.value,
-      password: this.password.value,
-      phoneNumber: this.phoneNumber.value,
-      city: this.city.value,
-      province: this.province.value,
-      country: this.conuntry.value,
-      role: this.role.value,
-      createdAt: this.createdAt.value,
-      updatedAt: this.updatedAt.value
-    }
+  get id(): string {
+    return this._id.value
+  }
+
+  get firstName(): string {
+    return this._firstName.value
+  }
+
+  get lastName(): string {
+    return this._lastName.value
+  }
+
+  get email(): string {
+    return this._email.value
+  }
+
+  get password(): string {
+    // TODO: Encrypt password
+    return this._password.value
+  }
+
+  get phoneNumber(): number {
+    return this._phoneNumber.value
+  }
+
+  get city(): string {
+    return this._city.value
+  }
+
+  get province(): string {
+    return this._province.value
+  }
+
+  get conuntry(): string {
+    return this._conuntry.value
+  }
+
+  get role(): string {
+    return this._role.value
+  }
+
+  get createdAt(): Date {
+    return this._createdAt.value
+  }
+
+  get updatedAt(): Date {
+    return this._updatedAt.value
   }
 }
 
