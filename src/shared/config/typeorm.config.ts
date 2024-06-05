85773d3ed18dba731108a82bb6e98455075e15e7
Figaarillo <axel.leonardi.22@gmail.com>
2024-06-05 20:24:12 -0300

    refactor: pass from typeorm config form literal object to class

--- src/shared/config/typeorm.config.ts
+++ src/shared/config/typeorm.config.ts
@@ -1,20 +1,36 @@
 /* eslint-disable n/no-path-concat */
 import { DataSource, type DataSourceOptions } from 'typeorm'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 
-const Config: DataSourceOptions = {
-  type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
-  database: process.env.DATABASE_NAME,
-  synchronize: false,
-  migrationsRun: true,
-  logging: true,
-  entities: [`${__dirname}/../../**/*.model{.ts,.js}`],
-  migrations: [`${__dirname}/../migrations/*{.ts, .js}`]
+class TypeormConfig {
+  private readonly config: DataSourceOptions
+  private readonly AppDataSource: DataSource
+
+  constructor() {
+    this.config = this.setupConfig()
+    this.AppDataSource = new DataSource(this.config)
+  }
+
+  private setupConfig(): DataSourceOptions {
+    return {
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
+      synchronize: false,
+      migrationsRun: true,
+      logging: true,
+      entities: [`${__dirname}/../../**/*.model{.ts,.js}`],
+      migrations: [`${__dirname}/../migrations/*{.ts, .js}`],
+      namingStrategy: new SnakeNamingStrategy()
+    }
+  }
+
+  async initDBConnection(): Promise<DataSource> {
+    return await this.AppDataSource.initialize()
+  }
 }
 
-const AppDataSource = new DataSource(Config)
-
-export default AppDataSource
+export default TypeormConfig
