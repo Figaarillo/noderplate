211652e2259b941430e156018075fcb2fc23c245
Figaarillo <axel.leonardi.22@gmail.com>
2024-06-05 11:05:20 -0300

    rafactor: modify custom error logic

--- src/shared/domain/exceptions/unexpected-error.ts
+++ src/shared/domain/exceptions/unexpected-error.ts
@@ -1,9 +1,9 @@
-import ErrorMessages from '@user/domain/utils/error-messages/error-messages.util'
-import BaseException from './base.exception'
-
-class UnexpectedError extends BaseException {
-  constructor(errorDescription?: any) {
-    super(ErrorMessages.UNEXPECTED_ERROR, UnexpectedError.name, errorDescription ?? '')
+class UnexpectedError extends Error {
+  constructor(
+    readonly description: string,
+    readonly name: string = UnexpectedError.name
+  ) {
+    super(description)
   }
 }
 
