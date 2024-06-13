0ebe0cebf2c72aa14e0f7a99eefffa5b1afc47a9
Figaarillo <axel.leonardi.22@gmail.com>
2024-06-12 21:18:04 -0300

    feat: create init point

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import TypeormConfig from '@shared/config/typeorm.config'
+import BootstrapUser from '@user/user.bootstrap'
+import * as dotenv from 'dotenv'
+
+// Main
+;(async () => {
+  dotenv.config()
+
+  const dbConfig = new TypeormConfig()
+  const db = await dbConfig.initDBConnection()
+
+  BootstrapUser(db)
+})()
