7a587ae5d103dae49e72823b8a9d3c20e6bd0935
Figaarillo <axel.leonardi.22@gmail.com>
2024-06-19 20:37:43 -0300

    feat: modify functions to inti user router

--- src/user/user.bootstrap.ts
+++ src/user/user.bootstrap.ts
@@ -2,12 +2,16 @@ import { type DataSource } from 'typeorm'
 import type UserRepository from './domain/repository/user.repository'
 import UserInMemoryRepository from './infrastructure/repositories/in-memory/user.in-memory.repository'
 import UserHandler from './infrastructure/handler/user.handler'
+import { type FastifyInstance } from 'fastify'
+import UserRoute from './infrastructure/routes/user.route'
 
-function BootstrapUser(_db: DataSource): void {
+function BootstrapUser(_db: DataSource, router: FastifyInstance): void {
   const repository: UserRepository = new UserInMemoryRepository()
 
-  // eslint-disable-next-line no-new
-  new UserHandler(repository)
+  const handler = new UserHandler(repository)
+
+  const userRoute = new UserRoute(router, handler)
+  userRoute.setupRoutes()
 }
 
 export default BootstrapUser
