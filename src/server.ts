77323aa00028fd765b3d12f9882cc14165e35cdd
Figaarillo <axel.leonardi.22@gmail.com>
2024-06-19 20:36:12 -0300

    feat: init fastify server in main function

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,31 @@
-import 'module-alias/register'
+/* eslint-disable no-console */
+import 'module-alias/register' // Must import this module first
+import * as dotenv from 'dotenv'
+import FastifyConifg from '@shared/config/fastify.config'
 import TypeormConfig from '@shared/config/typeorm.config'
 import BootstrapUser from '@user/user.bootstrap'
-import * as dotenv from 'dotenv'
 
-// Main
+dotenv.config()
+const PORT = Number(process.env.PORT)
+
+/* Main */
 ;(async () => {
-  dotenv.config()
-
   const dbConfig = new TypeormConfig()
   const db = await dbConfig.initDBConnection()
 
-  BootstrapUser(db)
+  const fastify = new FastifyConifg().fastify
+
+  BootstrapUser(db, fastify)
+
+  fastify.get('/', async () => {
+    return 'Hello, World!'
+  })
+
+  try {
+    await fastify.listen({ port: PORT })
+    console.log(`Server is running!🔥 Go to http://localhost:${PORT}`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
 })()
