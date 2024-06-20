48903fdf4b8bc21d4e0faf6bebda71103b51148a
Figaarillo <axel.leonardi.22@gmail.com>
2024-06-20 01:53:19 -0300

    feat: add handler in user route

--- src/user/infrastructure/routes/user.route.ts
+++ src/user/infrastructure/routes/user.route.ts
@@ -1,6 +1,6 @@
-import { type FastifyRequest, type FastifyInstance } from 'fastify'
-import type UserHandler from '../handler/user.handler'
 import { type HTTPQueryParams } from '@shared/utils/http.utils'
+import { type FastifyInstance, type FastifyRequest } from 'fastify'
+import type UserHandler from '../handler/user.handler'
 
 class UserRoute {
   constructor(
@@ -12,9 +12,9 @@ class UserRoute {
     this.router.get('/users', async (req: FastifyRequest<{ Querystring: HTTPQueryParams }>, res) => {
       await this.handler.List(req, res)
     })
-    // this.router.get('/users/:id', async (req, res) => {
-    //   await this.handler.getUserById(req, res)
-    // })
+    this.router.get('/users/:id', async (req: FastifyRequest<{ Params: { id: string } }>, res) => {
+      await this.handler.GetByID(req, res)
+    })
     // this.router.post('/users', async (req, res) => {
     //   await this.handler.registerUser(req, res)
     // })
